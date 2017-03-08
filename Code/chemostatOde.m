319dffc194f52e39cacead76c3d580aad831a01e
Thulasi <Thulasi>
2017-03-08 12:11:39 -0500

    asdfsad

--- Code/chemostatOde.m
+++ Code/chemostatOde.m
@@ -1,25 +1,21 @@
-% y(1) is substrate 1
-% y(2) is substrate 2
-% y(3) is substrate 3
-function dydt = gradostatOde(t, y, s1_in, s2_in, Params)
+% y(1) is substrate
+% y(2) is x1
+% y(3) is x2
+% y(4) is P
+function dydt = chemostatOde(t, y, s_in, Params)
 
-    fA = @funcAnt;
+    g = @funcInhib;
     fs_in = @funcSubIn;
     fs = @funcSub;
     
-    dydt(1)   =   Params.D * (fs_in(s1_in) - y(1)) - fs(Params, y(1)) * y(3);              %ds11/dt
+    dydt(1)   =   Params.D * (fs_in(s_in) - y(1)) - ...
+                        (1/Params.Y1)*fs( y(1), Params.K1, Params.mu1) * y(2) - ...
+                                (1/Params.Y2)*fs(y(1), Params.K2, Params.mu2)*y(3);       %ds/dt
     
-    dydt(2)   =   Params.D * (fs_in(s2_in) - y(2)) - fs(Params, y(1), y(2)) * y(3);        %ds12/dt
+    dydt(2)   =   fs(y(1), Params.K1, Params.mu1)*y(2) - ...
+                            Params.D * y(2) - g(Params) * y(2);        %dx1/dt
     
-    dydt(3)   =   Params.Y1 * fs(Params, y(1))*y(3) + ...
-                        Params.Y2 * fs(Params, y(1), y(2)) * y(3) - ...
-                            Params.D * y(3) - fA(Params) * y(3);                           %dx11/dt
+    dydt(3)   =   fs(y(1),Params.K2, Params.mu2)*y(3)  - ...
+                            Params.D * y(3);                           %dx2/dt
                         
-    dydt(4)   =   Params.D * (y(1) - y(4)) - fs(Params, y(4)) * y(6);                      %ds21/dt
-    
-    dydt(5)   =   Params.D * (y(2) - y(5)) - fs(Params, y(4), y(5)) * y(6);                %ds22/dt
-    
-    dydt(6)   =   Params.D * y(3) + Params.Y1 * fs(Params, y(4))*y(6) + ...
-                        Params.Y2 * fs(Params, y(4), y(5)) * y(6) - ...
-                            Params.D * y(6) - fA(Params) * y(6);                           %dx21/dt                        
-    dydt      =   dydt(:);
+    dydt(4)   =   fs(y(1),Params.)                %dp/dt
--- Code/main.m
+++ Code/main.m
@@ -5,7 +5,7 @@ function [] = main(filename)
 [ Params ] = loadGradParams(filename);
 
 options = odeset('AbsTol', Params.absTol, 'RelTol', Params.relTol);
-[t,y] = ode45(@(t,y)gradostatOde(t,y,Params.s1_in, Params.s2_in, Params), [0 Params.tfinal], [Params.s1_0 Params.s2_0 Params.x1_0 0 0 0], options);
+[t,y] = ode45(@(t,y)chemostatOde(t,y,Params.s1_in, Params.s2_in, Params), [0 Params.tfinal], [Params.s1_0 Params.s2_0 Params.x1_0 0 0 0], options);
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
